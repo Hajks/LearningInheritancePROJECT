4e890ecac30e1e8d8d5a1be7ba15b8dea6105caa
Hajk <tunczorx@gmail.com>
2018-04-05 09:23:33 +0200

    beginning of project

--- LearningInheritancePROJECT/Form1.cs
+++ LearningInheritancePROJECT/Form1.cs
@@ -10,11 +10,28 @@ using System.Windows.Forms;
 
 namespace LearningInheritancePROJECT
 {
+
     public partial class Form1 : Form
     {
+        Worker[] workers = new Worker[4];
+        Queen queen = new Queen();
         public Form1()
         {
             InitializeComponent();
+            workers[0] = new Worker (new string[] { "Zbieranie nektaru", "Wytwarzanie miodu" });
+            workers[1] = new Worker (new string[] { "Pielęgnacja jaj", "Nauczanie pszczółek" });
+            workers[2] = new Worker (new string[] { "Utrzymanie ula", "Patrol z żądłami" });
+            workers[3] = new Worker (new string[] { "Zbieranie nektaru", "Wytwarzanie miodu", "Pielęgnacja jaj", "Nauczanie pszczółek", "Utrzymanie ula", "Patrol z żądłami" });
+            queen = new Queen();
+
+        }
+
+        private void AssignWork(jobList.Items, numericUpDown1.Value);
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
         }
     }
 }
+
+
---
+++ LearningInheritancePROJECT/Worker.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningInheritancePROJECT
+{
+   public class Worker
+    {
+        private string[] jobsICanDo;
+        private int shiftsToWork;
+        private int shiftsWorked;
+    }
+}
